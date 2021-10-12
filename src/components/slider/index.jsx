97fadb81848dcfb8508460bfa11fc0382adc812a
alexxowo <alexandremarquez90@gmail.com>
2021-10-12 10:31:09 -0400

    Nuevos estilos. Ordenado componente SLIDER

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,22 +1,25 @@
 import React, {useState} from 'react'
 
+import './slider.css'
+
 const Slider = ({title, max, multiplier, type, onChangeValue}) => {
     
   const [value, setValue] = useState(0)
   const [consumeValue, setConsumeValue] = useState(0)
 
-  const handleChangeValue = (e) => {
+  const handleChangeValue = (e) => { 
     setValue(e.target.value);
-    setConsumeValue(e.target.value)
-    onChangeValue(e.target.value)
+    let consume = Number((e.target.value * multiplier).toFixed(2));
+    onChangeValue(consume);
+    setConsumeValue(consume);
   }
 
   return (
     <>
       <div className="slider_container">
-        <p>{title} - {value} {type}</p>
-        <input type="range" min="0" max={max} onChange={handleChangeValue} name="" id=""/>
-        <p>El consumo es: {value * multiplier}MB</p>
+        <p>{title} - {value} {type} / {consumeValue}MB</p>
+        <input type="range" min="0" max={max} value={value} onChange={handleChangeValue} name="" id=""/>
+        <span></span>
       </div>
     </>
   );
